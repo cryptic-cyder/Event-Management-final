4141432374c942323b62486322351d8886b756d9
Tahsin Azad Tias <tahsintias999@gmail.com>
2024-01-29 21:09:13 +0600

    up

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/repository/FeedbackRepoInterface.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/repository/FeedbackRepoInterface.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.OptBoolean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Optional;
+import java.util.Optional;
 
 public interface FeedbackRepoInterface extends JpaRepository<Feedback, Long> {
     Optional<Feedback> findByName(String name);
