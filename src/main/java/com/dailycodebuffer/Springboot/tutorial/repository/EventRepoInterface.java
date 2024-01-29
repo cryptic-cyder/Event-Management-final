3660b186376515c3d06df7cc6c446da18472f6c6
Tahsin Azad Tias <tahsintias999@gmail.com>
2024-01-29 21:08:28 +0600

    update

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/repository/EventRepoInterface.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/repository/EventRepoInterface.java
@@ -4,6 +4,7 @@ import com.dailycodebuffer.Springboot.tutorial.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Optional;
 
 public interface EventRepoInterface extends JpaRepository<Event, Long> {
     Optional<Event> findByEventName(String eventName);
